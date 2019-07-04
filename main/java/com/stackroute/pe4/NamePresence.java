e585606882822ca3b1f098b69086c3cfd2d3a8aa
AghaZafeer <51943287+AghaZafeer@users.noreply.github.com>
2019-07-04 11:12:30 +0530

    Update NamePresence.java

--- main/java/com/stackroute/pe4/NamePresence.java
+++ main/java/com/stackroute/pe4/NamePresence.java
@@ -8,8 +8,8 @@ public class NamePresence {
     public static boolean isNamePresent(String input,String regex)
     {
         Pattern pattern=Pattern.compile(regex);//pattern for given word
-        Matcher matcher=pattern.matcher(input);//
-        boolean matchfound=matcher.find();
+        Matcher matcher=pattern.matcher(input);//matcher object created
+        boolean matchfound=matcher.find();//finds the word in given input
         return matchfound;
     }
 }
